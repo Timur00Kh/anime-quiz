979a5581b670ff1f2d8edae179491fd9f9ac19c4
Timur <32297275+Timur00Kh@users.noreply.github.com>
2025-05-25 08:01:10 +0300

    Update route.ts

--- src/app/api/telegram-hook/route.ts
+++ src/app/api/telegram-hook/route.ts
@@ -22,7 +22,7 @@ bot.on("callback_query", async (ctx) => {
   if (ctx.callbackQuery.game_short_name === GAME_SHORT_NAME) {
     // Telegram will handle the WebApp opening
     await ctx.answerGameQuery(
-      "https://anime-quiz-virid.vercel.app/ost-quiz-tg"
+      `https://anime-quiz-virid.vercel.app/ost-quiz-tg#chat_id=${ctx.callbackQuery.message?.chat.id}&message_id=${ctx.callbackQuery.message?.message_id}`
     );
   } else {
     await ctx.answerCbQuery("Unknown game.", { show_alert: true });
